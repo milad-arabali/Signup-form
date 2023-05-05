5259fad0e0d6723beddfec1f6268f6a2ebb0753b
milad <miladarabali1@gmail.com>
2023-05-05 16:37:54 +0330

    ccs styles

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,6 +3,7 @@ import { validate } from './validate';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { notify } from './toast';
+import Styles from "./SignUp.module.css"
 
 const SignUp = () => {
     const[data,setData]= useState({
@@ -55,40 +56,41 @@ const SignUp = () => {
         }
     return (
         <div>   
-            <form onSubmit={submitHandler}>
-              <h1>SignUp</h1>
-                    <div>
+            <form onSubmit={submitHandler} className={Styles.formContainer}>
+              <h1 className={Styles.header}>SignUp</h1>
+                    <div  className={Styles.formField}>
                         <label>Name</label>
-                        <input type='text' name="Name" value={data.Name} onChange={changehander} onFocus={focushandler}/>                    
+                        <input  className={(useerror.Name && focuserror.Name) ? Styles.uncompleted : Styles.formInput} type='text' name="Name" value={data.Name} onChange={changehander} onFocus={focushandler}/>                    
                         {useerror.Name && focuserror.Name && <span>{useerror.Name}</span>}
                     </div>
-                    <div>
+                    <div className={Styles.formField}>
                         <label>Email</label>
-                        <input type='text' name="Email" value={data.Email} onChange={changehander}  onFocus={focushandler}/>                    
+                        <input className={(useerror.Email && focuserror.Email) ? Styles.uncompleted : Styles.formInput} type='text' name="Email" value={data.Email} onChange={changehander}  onFocus={focushandler}/>                    
                         {useerror.Email && focuserror.Email && <span>{useerror.Email}</span>}
 
                     </div>
-                    <div>
+                    <div className={Styles.formField}>
                         <label>Password</label>
-                        <input type='password' name="Password" value={data.Password} onChange={changehander} onFocus={focushandler}/>                    
+                        <input className={(useerror.Password && focuserror.Password) ? Styles.uncompleted : Styles.formInput} type='password' name="Password" value={data.Password} onChange={changehander} onFocus={focushandler}/>                    
                         {useerror.Password && focuserror.Password && <span>{useerror.Password}</span>}
 
                     </div>
-                    <div>
+                    <div className={Styles.formField}> 
                         <label>ConfrimPassword</label>
-                        <input type='Password' name="ConfrimPassword" value={data.ConfrimPassword} onChange={changehander} onFocus={focushandler}/>                    
+                        <input className={(useerror.ConfrimPassword && focuserror.ConfrimPassword) ? Styles.uncompleted : Styles.formInput} type='Password' name="ConfrimPassword" value={data.ConfrimPassword} onChange={changehander} onFocus={focushandler}/>                    
                         {useerror.ConfrimPassword && focuserror.ConfrimPassword && <span>{useerror.ConfrimPassword}</span>}
 
                 
                     </div>
-                    <div>
-                        <label>I accepred terms privacy policy</label>   
-                                          
-                        <input type='checkbox' name="IsAccepted" value={data.IsAccepted} onChange={changehander} onFocus={focushandler}/>                    
+                    <div className={Styles.formField}>
+                        <div className={Styles.checkBoxContainer}>
+                            <label>I accepred terms privacy policy</label>          
+                            <input className={(useerror.IsAccepted && focuserror.IsAccepted) ? Styles.uncompleted : Styles.formInput} type='checkbox' name="IsAccepted" value={data.IsAccepted} onChange={changehander} onFocus={focushandler}/>                    
+                        </div>
                         {useerror.IsAccepted && focuserror.IsAccepted && <span>{useerror.IsAccepted}</span>}
                         
                     </div>
-                    <div>
+                    <div className={Styles.formButtons} >
                         <a href="#">Login</a>
                         <button  type='submit'>SignUp</button>
                     </div>
